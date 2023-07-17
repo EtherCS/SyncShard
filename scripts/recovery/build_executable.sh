91f614524536e320865ee4091ffdd789692039f3
Ether <antfinancialyujian@gmail.com>
2023-07-17 14:53:23 -0400

    update script

--- scripts/recovery/build_executable.sh
+++ scripts/recovery/build_executable.sh
@@ -5,7 +5,7 @@ ROOT=$GOSRC/github.com/EtherCS/SyncShard
 
 mkdir -p build
 go build -o build/recnode $ROOT/abci/cmd/recnode/example    # compiler node
-go build -o build/monitor $ROOT/cmd/recover/monitor_client # client for detection
+# go build -o build/monitor $ROOT/cmd/recover/monitor_client # client for detection
 go build -o build/user $ROOT/cmd/recover/user_client    # user for sending tx
 go build -o build/latency $ROOT/cmd/recover/latency_client  # client for latency test
 
